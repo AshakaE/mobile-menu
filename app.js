278b53c2c66e2f3321bdb513a0a7fceb1bb72a55
AshakaE <ashakaegerega@gmail.com>
2021-07-01 07:31:18 +0100

    Fix linters

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const menu = document.querySelector('.container');
 const links = document.querySelector('#desktop-nav');
 
-menu.addEventListener("click", function() {
-  menu.classList.toggle("change");
+menu.addEventListener('click', () => {
+  menu.classList.toggle('change');
 
-  if (links.style.display === "block") {
-    links.style.display = "none";
+  if (links.style.display === 'block') {
+    links.style.display = 'none';
   } else {
-    links.style.display = "block";
+    links.style.display = 'block';
   }
 });
